30286ec00eaddb229f94c1898b4f36afeccdcaae
ravendraksingh <ravendraksingh@gmail.com>
2023-12-26 14:06:47 +0530

    added dynamo db config

--- src/errors/Api404Error.js
+++ src/errors/Api404Error.js
@@ -1,11 +1,11 @@
-const BaseError = require("./baseError");
+const BaseError = require("./BaseError");
 const errorTypes = require("./errorTypes");
 
 class Api404Error extends BaseError {
   constructor(message) {
     super(404, errorTypes.not_found_error, "NFE00CAT01", message);
-        Object.setPrototypeOf(this, BaseError.prototype);
-        this.name = "Api404Error";
+    Object.setPrototypeOf(this, BaseError.prototype);
+    this.name = "Api404Error";
   }
 }
 
